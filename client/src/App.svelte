83dc19f270905c9bf5a4f5c785ad4a9a73972f8b
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-03 23:52:54 +0100

    Initial svelte project setup

---
+++ client/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Tailwind from "./Tailwind.svelte";
+
+	export let name: string;
+</script>
+
+<main>
+  <Tailwind />
+	<h1 class="bg-blue-500">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
