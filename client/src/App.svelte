50f1cfce792fda37c07a3da96710c8e8911877cd
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-07 22:33:02 +0100

    Add some color

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -5,12 +5,12 @@
   import Tailwind from "./Tailwind.svelte";
 </script>
 
-<div class="max-w-[900px] m-4 md:mx-auto md:mt-16">
+<div class="max-w-[1000px] m-4 md:mx-auto md:mt-16">
   <Navigation />
   <main class="flex flex-col md:flex-row gap-8 mt-8">
-    <div class="order-2 md:order-1 w-[550px]">
+    <section class="order-2 md:order-1">
       <Documentation />
-    </div>
+    </section>
     <section class="order-1 md:order-2 card md:max-w-[350px]">
       <h1 class="text-xl">Sign in</h1>
       <p class="pt-2 text-zinc-700">To authenticate You, please enter Your account identifier.</p>
--- client/src/Documentation.svelte
+++ client/src/Documentation.svelte
@@ -1,5 +1,5 @@
 <template>
   <h1 class="text-4xl">WebAuthn</h1>
-  <p>This is a demo of the WebAuthn specification.</p>
+  <p>This is a demo of the WebAuthn specification. Bla bla la blabla blabla blab blablab bla blablab blablab</p>
   <p>This is a demo of the WebAuthn specification.</p>
 </template>
