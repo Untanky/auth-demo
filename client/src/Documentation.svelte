bc9a973dc0100a4fb471e9ab246fc681dd44dae9
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-18 15:30:42 +0100

    Use vite instead of rollup

--- client/src/Documentation.svelte
+++ client/src/Documentation.svelte
@@ -1,5 +1,34 @@
 <template>
-  <h1 class="text-4xl">WebAuthn</h1>
+  <h1>WebAuthn</h1>
+  <h2>WebAuthn</h2>
+  <h3>WebAuthn</h3>
+  <h4>WebAuthn</h4>
   <p>This is a demo of the WebAuthn specification. Bla bla la blabla blabla blab blablab bla blablab blablab</p>
   <p>This is a demo of the WebAuthn specification.</p>
 </template>
+
+<style>
+h1, h2, h3, h4, p {
+  @apply mb-2
+}
+
+h1 {
+  @apply text-2xl font-bold lg:text-3xl;
+}
+
+h2 {
+  @apply text-lg font-bold lg:text-2xl;
+}
+
+h3 {
+  @apply text-base font-bold lg:text-xl;
+}
+
+h4 {
+  @apply text-base font-semibold lg:text-lg;
+}
+
+p {
+  @apply text-zinc-300;
+}
+</style>
