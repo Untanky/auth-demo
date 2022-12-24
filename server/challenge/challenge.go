69fdb15e250f6dac809c3d9ecda8fb8323d038d0
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-24 10:38:28 +0100

    Rework challenge logic a bit

--- server/challenge/challenge.go
+++ server/challenge/challenge.go
@@ -2,9 +2,13 @@ package challenge
 
 import (
 	"math/rand"
+
+	"github.com/Untanky/iam-auth/oauth2"
 )
 
 const challengeAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+type challengeKey string
 
 func randStringBytes(n int) string {
 	b := make([]byte, n)
@@ -14,11 +18,35 @@ func randStringBytes(n int) string {
 	return string(b)
 }
 
-func GenerateChallengeKey() string {
-	return randStringBytes(32)
+func generateChallengeKey() challengeKey {
+	return challengeKey(randStringBytes(32))
 }
 
 // A stateful authorization challenge
-type Challenge struct {
-	Value string
+type challenge struct {
+	key           challengeKey
+	authorization *oauth2.AuthorizationRequest
+}
+
+func (challenge *challenge) BindAuthorizationState(request *oauth2.AuthorizationRequest) {
+	challenge.authorization = request
+}
+
+type ChallengeRepository interface {
+	FindChallengeByKey(key challengeKey) (*challenge, error)
+	CreateChallenge() (*challenge, error)
+	UpdateChallenge(*challenge) error
+	DeleteChallenge(*challenge) error
+	DeleteChallengeByKey(key challengeKey) error
+}
+
+func GetAuthorizationStateFrom(repo ChallengeRepository) oauth2.GetAuthorizationState {
+	return func(key string) (*oauth2.AuthorizationRequest, error) {
+		challenge, err := repo.FindChallengeByKey(challengeKey(key))
+
+		if err != nil {
+			return nil, err
+		}
+		return challenge.authorization, nil
+	}
 }
