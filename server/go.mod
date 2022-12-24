09db386b61409de37e686faca98e7b7618df782c
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-24 11:23:59 +0100

    Implement validation oauth validatation logic

--- server/go.mod
+++ server/go.mod
@@ -22,8 +22,9 @@ require (
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- server/oauth2/authorize_controller.go
+++ server/oauth2/authorize_controller.go
@@ -3,8 +3,11 @@ package oauth2
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/slices"
 )
 
 const AuthenticationEndpoint = "/"
@@ -19,19 +22,20 @@ type AuthorizationState interface {
 }
 
 type AuthorizationController struct {
+	clientRepo         ClientRepository
 	authorizationState AuthorizationState
 }
 
 func (controller *AuthorizationController) StartAuthorization(c *gin.Context) {
 	var request AuthorizationRequest
 	if err := c.BindQuery(&request); err != nil {
-		controller.FailAuthorization(&request, invalid_request, c)
+		controller.failAuthorization(&request, invalid_request, c)
 		return
 	}
 
 	challenge, err := controller.authorizationState.Set(&request)
 	if err != nil {
-		controller.FailAuthorization(&request, server_error, c)
+		controller.failAuthorization(&request, server_error, c)
 		fmt.Println(err)
 		return
 	}
@@ -40,6 +44,33 @@ func (controller *AuthorizationController) StartAuthorization(c *gin.Context) {
 }
 
 func (controller *AuthorizationController) FinishAuthorization(request *AuthorizationRequest, c *gin.Context) {
+	client, err := controller.clientRepo.FindClient(request.ClientID)
+	if err != nil {
+		controller.failAuthorization(request, unauthorized_client, c)
+		return
+	}
+
+	if !slices.Contains(client.ResponseTypes, request.ResponseType) {
+		controller.failAuthorization(request, invalid_request, c)
+		return
+	}
+
+	if request.RedirectURI == "" {
+		request.RedirectURI = client.RedirectionURIs[0]
+	} else if !slices.ContainsFunc(client.RedirectionURIs, func(uri string) bool { return uri == request.RedirectURI }) {
+		controller.failAuthorization(request, invalid_request, c)
+		return
+	}
+
+	redirectionUrl, err := url.Parse(request.RedirectURI)
+	if err != nil {
+		controller.failAuthorization(request, invalid_request, c)
+		return
+	}
+
+	if !redirectionUrl.Query().Has("state") {
+		redirectionUrl.Query().Add("state", request.State)
+	}
 
 	switch request.ResponseType {
 	case ResponseTypeCode:
@@ -48,7 +79,7 @@ func (controller *AuthorizationController) FinishAuthorization(request *Authoriz
 			State: request.State,
 		}
 
-		c.Redirect(http.StatusFound, fmt.Sprintf("%s?code=%s&state%s", request.RedirectURI, response.Code, response.State))
+		redirectionUrl.Query().Add("code", response.Code)
 		break
 	case ResponseTypeToken:
 		response := &TokenResponse{
@@ -59,21 +90,23 @@ func (controller *AuthorizationController) FinishAuthorization(request *Authoriz
 			State:       request.State,
 		}
 
-		c.Redirect(http.StatusFound, fmt.Sprintf(
-			"%s?access_token=%s&expires_in=%d&token_type=%s&scope=%s&state%s",
-			request.RedirectURI,
-			response.AccessToken,
-			response.ExpiresIn,
-			response.TokenType,
-			response.Scope,
-			response.State,
-		))
+		redirectionUrl.Query().Add("access_token", response.AccessToken)
+		redirectionUrl.Query().Add("scope", strings.Join(request.Scope, ""))
+		redirectionUrl.Query().Add("expires_in", fmt.Sprint(response.ExpiresIn))
+		redirectionUrl.Query().Add("token_type", response.TokenType)
 		break
 	default:
-		controller.FailAuthorization(request, unsupported_response_type, c)
+		controller.failAuthorization(request, unsupported_response_type, c)
+		return
 	}
+
+	c.Redirect(http.StatusFound, fmt.Sprint(redirectionUrl))
 }
 
-func (controller *AuthorizationController) FailAuthorization(request *AuthorizationRequest, err oauth2Error, c *gin.Context) {
+func (controller *AuthorizationController) failAuthorization(request *AuthorizationRequest, err oauth2Error, c *gin.Context) {
+
+}
+
+func (controller *AuthorizationController) RedirectFailAuthorization(request *AuthorizationRequest, err oauth2Error, c *gin.Context) {
 
 }
