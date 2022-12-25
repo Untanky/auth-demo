c4eb907120d982ade65abbe50a2ddd5a93515a6e
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-25 19:09:08 +0100

    Implement basic auth

--- server/oauth2/token_controller.go
+++ server/oauth2/token_controller.go
@@ -24,7 +24,9 @@ func (controller *TokenController) CreateAccessToken(c *gin.Context) {
 	}
 
 	client, _ := controller.clientRepo.FindClient(request.ClientId)
-	controller.authenticate(client, c)
+	if !controller.authenticate(client, c) {
+		return
+	}
 
 	tokenResponse := TokenResponse{
 		TokenType: "Bearer",
@@ -45,12 +47,20 @@ func (controller *TokenController) CreateAccessToken(c *gin.Context) {
 		if err != nil {
 			controller.logger.Error(fmt.Sprintf("Authorization code not found: %s", err))
 			controller.failAuthorization(InvalidGrant, c)
+            return
 		}
 
 		if authorizationRequest.RedirectURI != authorizationCodeRequest.RedirectURI {
 			controller.logger.Error("Redirect URIs do not match between authorize and token endpoint")
 			controller.failAuthorization(InvalidGrant, c)
+            return
 		}
+
+        if authorizationRequest.ClientID != client.ID {
+            controller.logger.Error(fmt.Sprintf("Authorization code was issued to another client (request client: %s, code client: %s)", authorizationRequest.ClientID, client.ID))
+            controller.failAuthorization(InvalidGrant, c)
+            return
+        }
 
 		tokenResponse.State = authorizationRequest.State
 	case GrantTypeRefreshToken:
@@ -76,35 +86,66 @@ func (controller *TokenController) CreateAccessToken(c *gin.Context) {
 
 func (controller *TokenController) authenticate(client *Client, c *gin.Context) bool {
     if client == nil {
-        basicClient, ok := controller.authenticateWithBasicAuth(c)
-        client = basicClient
-        return ok
-    }
-
-	switch client.AuthenticationMethod {
-	case ClientSecretBasic:
-        basicClient, ok := controller.authenticateWithBasicAuth(c)
-        if basicClient.ID != client.ID {
-            controller.logger.Error("Client from basic auth and request params do not match")
+        clientAuthentication := c.Request.Header.Get("Authorization")
+        basicClient, err := controller.authenticateWithBasicAuth(clientAuthentication)
+        if err != nil {
+            controller.failAuthorization(*err, c)
             return false
         }
-        return ok
+		client = basicClient
+		return true
+	}
+
+	switch client.AuthenticationMethod {
+    case ClientSecretBasic:
+        clientAuthentication := c.Request.Header.Get("Authorization")
+		basicClient, err := controller.authenticateWithBasicAuth(clientAuthentication)
+        if err != nil {
+            controller.failAuthorization(*err, c)
+            return false
+        }
+		if basicClient.ID != client.ID {
+			controller.logger.Error("Client from basic auth and request params do not match")
+            controller.failAuthorization(InvalidClient, c)
+			return false
+		}
+		return true
 	case ClientAuthenticationNone:
 		return true
-	case ClientSecretPost:
-		fallthrough
-	case ClientPrivateKey:
-		fallthrough
-	case ClientSecretJWT:
-		controller.logger.Error("Client cannot be authenticated")
-		c.Header("WWW-Authenticate", "Basic")
-		controller.failAuthorization(InvalidClient, c)
-		return false
+
 	}
+
+	controller.logger.Error("Client cannot be authenticated")
+	c.Header("WWW-Authenticate", "Basic")
+	controller.failAuthorization(InvalidClient, c)
+	return false
 }
 
-func (controller *TokenController) authenticateWithBasicAuth(c *gin.Context) (*Client, bool) {
+func (controller *TokenController) authenticateWithBasicAuth(clientAuthentication string) (*Client, *OAuth2Error) {
+	if !strings.HasPrefix("Basic", clientAuthentication) {
+		return nil, &InvalidClient
+	}
+	basicAuthentication := clientAuthentication[6:]
+	decodedBasicAuth, err := base64.RawURLEncoding.DecodeString(basicAuthentication)
+	if err != nil {
+		controller.logger.Error(fmt.Sprintf("Client cannot be authenticated"))
+        return nil, &InvalidClient
+	}
+	basicAuthComponents := strings.Split(string(decodedBasicAuth), ":")
+	clientID, password := clientID(basicAuthComponents[0]), basicAuthComponents[1]
 
+	client, err := controller.clientRepo.FindClient(clientID)
+	if err != nil {
+		controller.logger.Error(fmt.Sprintf("Client could not be found: %s", err))
+        return nil, &InvalidClient
+	}
+
+    if string(client.Secret) != password {
+        controller.logger.Error(fmt.Sprintf("Client secret does not match: %s", err))
+        return nil, &InvalidClient
+    }
+
+	return client, nil
 }
 
 func (controller *TokenController) failAuthorization(err OAuth2Error, c *gin.Context) {
