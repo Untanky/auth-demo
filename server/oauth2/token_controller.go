e8707b483b41fd0e4eaa8d813a3af4d1a3e8b4b2
Lukas Grimm <lukaskingsmail@gmail.com>
2022-12-25 18:34:55 +0100

    First implementation attempt at client authentication

--- server/oauth2/token_controller.go
+++ server/oauth2/token_controller.go
@@ -1,10 +1,12 @@
 package oauth2
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/Untanky/iam-auth/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 type TokenController struct {
@@ -21,13 +23,40 @@ func (controller *TokenController) CreateAccessToken(c *gin.Context) {
 		return
 	}
 
-    // TODO: Authenticate client
+    var client *Client
+	{
+		clientAuthentication := c.Request.Header.Get("Authorization")
+		if !strings.HasPrefix("Basic", clientAuthentication) {
+			controller.logger.Error("Client cannot be authenticated")
+			c.Header("WWW-Authenticate", "Basic")
+			controller.failAuthorization(InvalidClient, c)
+			return
+		}
+		basicAuthentication := clientAuthentication[6:]
+		decodedBasicAuth, err := base64.RawURLEncoding.DecodeString(basicAuthentication)
+		if err != nil {
+            controller.logger.Error(fmt.Sprintf("Client cannot be authenticated"))
+			controller.failAuthorization(InvalidClient, c)
+			return
+		}
+        basicAuthComponents := strings.Split(string(decodedBasicAuth), ":")
+        clientID, password := clientID(basicAuthComponents[0]), basicAuthComponents[1]
 
-    tokenResponse := TokenResponse{
-        TokenType: "Bearer",
-        ExpiresIn: 60 * 60,
-        Scope:     request.Scope,
-    }
+        client, err = controller.clientRepo.FindClient(clientID)
+        if err != nil {
+            controller.logger.Error(fmt.Sprintf("Client could not be found: %s", err))
+            controller.failAuthorization(InvalidClient, c)
+            return
+        }
+
+        if client.Secret
+	}
+
+	tokenResponse := TokenResponse{
+		TokenType: "Bearer",
+		ExpiresIn: 60 * 60,
+		Scope:     request.Scope,
+	}
 
 	switch request.GrantType {
 	case GrantTypeAuthorizationCode:
@@ -38,32 +67,35 @@ func (controller *TokenController) CreateAccessToken(c *gin.Context) {
 			return
 		}
 
-        authorizationRequest, err := controller.codeState.Get(authorizationCodeRequest.Code)
-        if err != nil {
-            controller.logger.Error(fmt.Sprintf("Authorization code not found: %s", err))
-            controller.failAuthorization(InvalidGrant, c)
-        }
+		authorizationRequest, err := controller.codeState.Get(authorizationCodeRequest.Code)
+		if err != nil {
+			controller.logger.Error(fmt.Sprintf("Authorization code not found: %s", err))
+			controller.failAuthorization(InvalidGrant, c)
+		}
 
-        if authorizationRequest.RedirectURI != authorizationCodeRequest.RedirectURI {
-            controller.logger.Error("Redirect URIs do not match between authorize and token endpoint")
-            controller.failAuthorization(InvalidGrant, c)
-        }
+		if authorizationRequest.RedirectURI != authorizationCodeRequest.RedirectURI {
+			controller.logger.Error("Redirect URIs do not match between authorize and token endpoint")
+			controller.failAuthorization(InvalidGrant, c)
+		}
 
-        tokenResponse.State = authorizationRequest.State
+		tokenResponse.State = authorizationRequest.State
 		break
 	case GrantTypeRefreshToken:
-		var refreshTokenRequest AuthorizationCodeTokenRequest
+		var refreshTokenRequest RefreshTokenRequest
 		if err := c.BindQuery(&refreshTokenRequest); err != nil {
 			controller.logger.Error(fmt.Sprintf("Cannot parse query parameters: %s", err))
 			controller.failAuthorization(InvalidRequest, c)
 			return
 		}
+
+		// TODO: check if refresh token is valid
+		// TODO: check if refresh token was authored to the client
 		break
 	case GrantTypePassword:
 	default:
 		controller.logger.Error(fmt.Sprintf("Grant type (%s) not supported", request.GrantType))
-        controller.failAuthorization(UnsupportedGrantType, c)
-        return
+		controller.failAuthorization(UnsupportedGrantType, c)
+		return
 	}
 
 	c.JSON(http.StatusOK, tokenResponse)
