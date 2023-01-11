c60ae9007ac6210bba01550c5ad77a4e380624b8
Lukas Grimm <lukaskingsmail@gmail.com>
2023-01-11 14:50:58 +0100

    Improve spacing a bit

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="max-w-[512px] m-32 px-4 mx-auto">
-  <main class="card p-8 md:p-12">
+  <main class="pt-8 pb-8 px-16 md:pt-12 md:pb-24 md:px-24 rounded-xl border-2 border-zinc-200 dark/:border-zinc-600 dark/:bg-black">
     <SmartWebauthn />
   </main>
 </div>
